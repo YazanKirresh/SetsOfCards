20c6f35e729a1ab834e29c16a53cea11599a24b3
Yazan <yazan.kirresh@hotmail.com>
2024-08-18 22:17:27 +0300

    Updated the input to include all the cards and added a test value to check the size of each set if it has 13 card then we can create a set

--- src/App.java
+++ src/App.java
@@ -14,46 +14,123 @@ public class App {
                 switch (arrayOfCards[i][1]){
                     case "S":
                         if (SetOfSpades.containsKey(arrayOfCards[i][j])){
-                        // we already have it, we need to increment the send value of the map
-                        int duplicate = SetOfSpades.get(arrayOfCards[i][j]);
-                        duplicate++;
-                        SetOfSpades.put(arrayOfCards[i][0],duplicate);
+                            // we already have it this card, we need to increment the second value value of the map
+                            int duplicate = SetOfSpades.get(arrayOfCards[i][j]);
+                            System.out.println("Current: "+ arrayOfCards[i][j]);
+                            //System.out.println("Spades cards:"+ SetOfSpades);
+                            //System.out.println("Duplicates: "+ duplicate);
+                            duplicate++;
+                            //System.out.println("Duplicates: "+ duplicate);
+                            SetOfSpades.put(arrayOfCards[i][0],duplicate);
+                            System.out.println("Spades cards:"+ SetOfSpades);
+
                         }else{
                             SetOfSpades.put(arrayOfCards[i][0],1);
                         }
                         break;
                     case "C":
+                        if (SetOfClubs.containsKey(arrayOfCards[i][j])){
+                            // we already have it this card, we need to increment the second value value of the map
+                            int duplicate = SetOfClubs.get(arrayOfCards[i][j]);
+                            duplicate++;
+                            SetOfClubs.put(arrayOfCards[i][0],duplicate);
+                        }else{
+                            SetOfClubs.put(arrayOfCards[i][0],1);
+                        }
                         break;
                     case "D":
+                    if (SetOfDiamonds.containsKey(arrayOfCards[i][j])){
+                        // we already have it this card, we need to increment the second value value of the map
+                        int duplicate = SetOfDiamonds.get(arrayOfCards[i][j]);
+                        duplicate++;
+                        SetOfDiamonds.put(arrayOfCards[i][0],duplicate);
+                    }else{
+                        SetOfDiamonds.put(arrayOfCards[i][0],1);
+                    }
                         break;
                     case "H":
+                    if (SetOfHearts.containsKey(arrayOfCards[i][j])){
+                        // we already have it this card, we need to increment the second value value of the map
+                        int duplicate = SetOfHearts.get(arrayOfCards[i][j]);
+                        duplicate++;
+                        SetOfHearts.put(arrayOfCards[i][0],duplicate);
+                    }else{
+                        SetOfHearts.put(arrayOfCards[i][0],1);
+                    }
                         break;
                 }
  
             }
         }   
+        if (SetOfSpades.size() == 13 && SetOfClubs.size() == 13 && SetOfDiamonds.size() == 13 && SetOfHearts.size() == 13 ){
+            amountOfSets++;
+            // We can create at least a set of cards
+        }
         System.out.println("Spades cards:"+ SetOfSpades);
+        System.out.println(SetOfSpades.size());
         System.out.println("Clubs cards:"+ SetOfClubs);
+        System.out.println(SetOfClubs.size());
         System.out.println("Diamond cards:"+ SetOfDiamonds);
+        System.out.println(SetOfDiamonds.size());
         System.out.println("Hearts cards:"+ SetOfHearts);
+        System.out.println(SetOfHearts.size());
         return amountOfSets;
     }
 
     public static void main(String[] args) throws Exception { 
         String[][] myCards = {
             {"0", "S"},
+            {"0", "H"},
+            {"0", "D"},
+            {"0", "C"},
+            {"1", "S"},
             {"1", "H"},
+            {"1", "D"},
+            {"1", "C"},
+            {"2", "S"},
+            {"2", "H"},
             {"2", "D"},
+            {"2", "C"},
+            {"3", "S"},
+            {"3", "H"},
+            {"3", "D"},
             {"3", "C"},
             {"4", "S"},
+            {"4", "H"},
+            {"4", "D"},
+            {"4", "C"},
+            {"5", "S"},
             {"5", "H"},
+            {"5", "D"},
+            {"5", "C"},
+            {"6", "S"},
+            {"6", "H"},
             {"6", "D"},
+            {"6", "C"},
+            {"7", "S"},
+            {"7", "H"},
+            {"7", "D"},
             {"7", "C"},
             {"8", "S"},
+            {"8", "H"},
+            {"8", "D"},
+            {"8", "C"},
+            {"9", "S"},
             {"9", "H"},
+            {"9", "D"},
+            {"9", "C"},
+            {"10", "S"},
+            {"10", "H"},
             {"10", "D"},
+            {"10", "C"},
+            {"11", "S"},
+            {"11", "H"},
+            {"11", "D"},
             {"11", "C"},
             {"12", "S"},
+            {"12", "H"},
+            {"12", "D"},
+            {"12", "C"},
         };
         // input is two dimensional array
         int cardSets = FindHowManyCardSets(myCards);
